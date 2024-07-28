d14d018e93fed3c59489b5b8147497edcb6cfb2f
alancdh <84582587+alancdh@users.noreply.github.com>
2024-07-28 12:31:15 -0600

    Update DemoBankApplicationTests.java

--- DemoBank/src/test/java/com/example/DemoBank/DemoBankApplicationTests.java
+++ DemoBank/src/test/java/com/example/DemoBank/DemoBankApplicationTests.java
@@ -1,33 +1,26 @@
 package com.example.DemoBank;
-import net.bytebuddy.utility.RandomString;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
-import java.util.Random;
-
 
 @SpringBootTest
 class DemoBankApplicationTests {
-	private static final String Letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-	private static final int Longitud = 8;
 
-	public static String Cadena (String ) {
-		Random random = new Random();
-		StringBuilder creadorCadena = new StringBuilder(Longitud);
-		for (int i = 0; i < Longitud; i++) {
-			int contador = random.nextInt(Letras.length());
-			creadorCadena.append(Letras.charAt(contador));
-		}
+private WebDriver driver;
 
-	 	String randomString = creadorCadena.toString();
-	//	System.out.println("Cadena Random: " + randomString);
-        return randomString;
-    }
-
-	private WebDriver driver;
+	String cadenaName = generaRandom.FirstNameRandom();
+	String cadenaLast = generaRandom.LastNameRandom();
+	String AdressRandom = generaRandom.LastNameRandom();
+	String CityRandom = generaRandom.LastNameRandom();
+	String StateRandom = generaRandom.LastNameRandom();
+	String ZipCodeRandom = generaRandom.LastNameRandom();
+	String PhoneRandom = generaRandom.LastNameRandom();
+	String SnnRandom = generaRandom.LastNameRandom();
+	String UsernameRandom = generaRandom.LastNameRandom();
+	String PasswordRandom = generaRandom.LastNameRandom();
 
 	@BeforeEach
 	void getUp(){
@@ -37,32 +30,32 @@ class DemoBankApplicationTests {
 	@Test
 	void testBank() {
 		driver.get("https://parabank.parasoft.com/parabank/index.htm");
-
 		driver.findElement(By.xpath("//*[@id=\"loginPanel\"]/p[2]/a")).click();
 
 		//Ingresar datos para crear cuenta
-		driver.findElement(By.id("customer.firstName")).sendKeys(C);
-		driver.findElement(By.id("customer.lastName")).sendKeys();
-		driver.findElement(By.id("customer.address.street")).sendKeys();
-		driver.findElement(By.id("customer.address.city")).sendKeys();
-		driver.findElement(By.id("customer.address.state")).sendKeys();
-		driver.findElement(By.id("customer.address.zipCode")).sendKeys();
-		driver.findElement(By.id("customer.phoneNumber")).sendKeys();
-		driver.findElement(By.id("customer.ssn")).sendKeys();
-		driver.findElement(By.id("customer.customer.username")).sendKeys();
-		driver.findElement(By.id("customer.password")).sendKeys();
-		driver.findElement(By.id("repeatedPassword")).sendKeys();
-
+		driver.findElement(By.id("customer.firstName")).sendKeys(cadenaName);
+		driver.findElement(By.id("customer.lastName")).sendKeys(cadenaLast);
+		driver.findElement(By.id("customer.address.street")).sendKeys(AdressRandom);
+		driver.findElement(By.id("customer.address.city")).sendKeys(CityRandom);
+		driver.findElement(By.id("customer.address.state")).sendKeys(StateRandom);
+		driver.findElement(By.id("customer.address.zipCode")).sendKeys(ZipCodeRandom);
+		driver.findElement(By.id("customer.phoneNumber")).sendKeys(PhoneRandom);
+		driver.findElement(By.id("customer.ssn")).sendKeys(SnnRandom);
+		String Usuario = UsernameRandom;
+		driver.findElement(By.id("customer.username")).sendKeys(UsernameRandom);
+		String Contrasenia = PasswordRandom;
+		driver.findElement(By.id("customer.password")).sendKeys(PasswordRandom);
+		driver.findElement(By.id("repeatedPassword")).sendKeys(PasswordRandom);
 		driver.findElement(By.xpath("//*[@id=\"customerForm\"]/table/tbody/tr[13]/td[2]/input")).click();
 
 		if (driver.getPageSource().contains("Your account was created successfully. You are now logged in.")){
 			driver.findElement(By.xpath("//*[@id=\"leftPanel\"]/ul/li[8]/a")).click();
-
-			driver.findElement(By.xpath("//*[@id=\"loginPanel\"]/form/div[1]/input")).sendKeys();
-			driver.findElement(By.xpath("//*[@id=\"loginPanel\"]/form/div[2]/input")).sendKeys();
+			driver.findElement(By.xpath("//*[@id=\"loginPanel\"]/form/div[1]/input")).sendKeys(Usuario);
+			driver.findElement(By.xpath("//*[@id=\"loginPanel\"]/form/div[2]/input")).sendKeys(Contrasenia);
 			driver.findElement(By.xpath("//*[@id=\"loginPanel\"]/form/div[3]/input")).click();
 		}else{
 			driver.close();
+			System.out.println("No se pudo completar el registro");
 		}
 
 	}
